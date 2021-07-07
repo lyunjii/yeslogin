7c94e744b0c33eb1356eb22a657cad035aa207b2
Yunji <yunji_18@naver.com>
2021-07-07 17:47:01 +0900

    change msg color

--- select.js
+++ select.js
@@ -103,5 +103,22 @@ function loadPic(input){
   newImage.style.filter = "none";
 
   document.querySelector('.upTxt').style.display = "none";
+
+import Cropper from 'cropperjs';
+const image = document.querySelector('.newPic');
+const cropper = new Cropper(image, {
+  aspectRatio: 16 / 9,
+  crop(event) {
+    console.log(event.detail.x);
+    console.log(event.detail.y);
+    console.log(event.detail.width);
+    console.log(event.detail.height);
+    console.log(event.detail.rotate);
+    console.log(event.detail.scaleX);
+    console.log(event.detail.scaleY);
+  },
+});
+
   */
 }
+
