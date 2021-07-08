3ffbccfdc2bd27fd0039d4e11e21e49ae6359d42
Yunji <yunji_18@naver.com>
2021-07-08 16:12:50 +0900

    use cropperjs...

--- select.js
+++ select.js
@@ -1,5 +1,5 @@
 window.onload = function (){
-  if(is_mobile() === false){
+  if(is_mobile() === true){
     var logo = document.querySelector('.logo');
     logo.style.width = "258px";
     logo.style.height = "104px"
@@ -70,7 +70,7 @@ function loadPic(input){
   var file = input.files[0];
 
   var newImage = document.createElement("img");
-  newImage.setAttribute("class", 'newPic');
+  newImage.setAttribute("id", 'newPic');
 
   newImage.src = URL.createObjectURL(file);
 
@@ -88,6 +88,8 @@ function loadPic(input){
     for(var i = 0; i < opt.length; i++){
       opt[i].style.display = "block";
     }
+
+  editImg();
 
   /*
   // change url of img
@@ -122,3 +124,25 @@ const cropper = new Cropper(image, {
   */
 }
 
+var TEMPLATE = '<div class="cropper-container" touch-action="none">' + '<div class="cropper-wrap-box">' + '<div class="cropper-canvas"></div>' + '</div>' + '<div class="cropper-drag-box"></div>' + '<div class="cropper-crop-box">' + '<span class="cropper-view-box"></span>' + '<span class="cropper-dashed dashed-h"></span>' + '<span class="cropper-dashed dashed-v"></span>' + '<span class="cropper-center"></span>' + '<span class="cropper-face"></span>' + '<span class="cropper-line line-e" data-cropper-action="e"></span>' + '<span class="cropper-line line-n" data-cropper-action="n"></span>' + '<span class="cropper-line line-w" data-cropper-action="w"></span>' + '<span class="cropper-line line-s" data-cropper-action="s"></span>' + '<span class="cropper-point point-e" data-cropper-action="e"></span>' + '<span class="cropper-point point-n" data-cropper-action="n"></span>' + '<span class="cropper-point point-w" data-cropper-action="w"></span>' + '<span class="cropper-point point-s" data-cropper-action="s"></span>' + '<span class="cropper-point point-ne" data-cropper-action="ne"></span>' + '<span class="cropper-point point-nw" data-cropper-action="nw"></span>' + '<span class="cropper-point point-sw" data-cropper-action="sw"></span>' + '<span class="cropper-point point-se" data-cropper-action="se"></span>' + '</div>' + '</div>';
+
+function editImg(){
+  const image = document.getElementById('newPic');
+  const cropper = new Cropper(image, {
+    viewMode: 2,
+    //dragMode: 'none',
+    aspectRatio: 16 / 9,
+    center: false,
+    highlight: false,
+    background: false,
+    autoCropArea: 1,
+    cropBoxResizable: false,
+    crop(event) {},
+  });
+
+  document.querySelector('.cropper-modal').style.opacity = "0.7";
+  var point = document.getElementsByClassName('cropper-point');
+  for(var i = 0; i < point.length; i++){
+    point[i].style.backgroundColor = "#FF8836";
+  }
+}
