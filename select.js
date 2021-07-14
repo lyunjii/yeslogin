c0ddaa3044b78c93eb46c12ae708200eb0b9cd8e
Yunji <yunji_18@naver.com>
2021-07-14 14:20:14 +0900

    editing

--- select.js
+++ select.js
@@ -49,7 +49,7 @@ window.onload = function (){
     sendIcon.style.height = "20px";
     sendIcon.style.marginRight = "10.88px";
 
-   // var cropPointer = document.querySelector('.cropper-point.point-se');
+    //var cropPointer = document.querySelector('.cropper-point.point-se');
     //cropPointer.style.width = "22px";
     //cropPointer.style.height = "22px";
     
@@ -62,8 +62,7 @@ function is_mobile()
     return true;
   }
   
-  if (typeof window.orientation !== 'undefined') 
-  {
+  if (typeof window.orientation !== 'undefined') {
     return true;
   }
   
@@ -80,61 +79,58 @@ var showPic = document.querySelector('.showPic');
 var result = document.querySelector('.result');
 var editBtn = document.querySelector('.editBtn');
 var toggle = document.querySelector('.toggle');
+var toggleBtn = document.querySelector('#toggleBtn')
 var msgBox = document.querySelector('.msgBox');
 
 function load_image(input){
-  upPicBox.style.display = "none";
+  if(!input.files[0]) return;
+  else{
+    upPicBox.style.display = "none";
 
-  var newImage = document.createElement("img");
-  newImage.setAttribute("id", 'newPic');
+    var newImage = document.createElement("img");
+    newImage.setAttribute("id", 'newPic');
 
-  var file = input.files[0];
-  newImage.src = URL.createObjectURL(file);
+    var file = input.files[0];
+    newImage.src = URL.createObjectURL(file);
 
-  showPic.appendChild(newImage);
+    showPic.appendChild(newImage);
 
-  showPic.style.display = "block";
-  picture.style.marginBottom = "0px";
-  var opt = document.getElementsByClassName('option');
-  for(var i = 0; i < opt.length; i++){
-    opt[i].style.display = "block";
+    showPic.style.display = "block";
+    picture.style.marginBottom = "0px";
+    var opt = document.getElementsByClassName('option');
+    for(var i = 0; i < opt.length; i++){
+      opt[i].style.display = "block";
+    }
+
+    newImage.style.width = "100%";
+    newImage.style.height = "100%";
+    newImage.style.objectFit = "contain";
+    //newImage.style.objectFit = "cover";
+    //container.style.overflow = "hidden";
+    newImage.style.filter = "none";
+    //newImage.style.maxWidth = "100%";
+
+    edit_image();
+
+    editBtn.style.display = "flex";
+    toggle.style.display = "flex";
+    if(!is_mobile()){ editBtn.style.top = "487px"; }
   }
-
-  newImage.style.width = "100%";
-  newImage.style.height = "100%";
-  newImage.style.objectFit = "contain";
-  //newImage.style.objectFit = "cover";
-  //container.style.overflow = "hidden";
-  newImage.style.filter = "none";
-  //newImage.style.maxWidth = "100%";
-
-  edit_image();
-
-  editBtn.style.display = "flex";
-  toggle.style.display = "flex";
-  if(!is_mobile()){ editBtn.style.top = "487px"; }
-
 }
-
-//var TEMPLATE = '<div class="cropper-container" touch-action="none">' + '<div class="cropper-wrap-box">' + '<div class="cropper-canvas"></div>' + '</div>' + '<div class="cropper-drag-box"></div>' + '<div class="cropper-crop-box">' + '<span class="cropper-view-box"></span>' + '<span class="cropper-dashed dashed-h"></span>' + '<span class="cropper-dashed dashed-v"></span>' + '<span class="cropper-center"></span>' + '<span class="cropper-face"></span>' + '<span class="cropper-line line-e" data-cropper-action="e"></span>' + '<span class="cropper-line line-n" data-cropper-action="n"></span>' + '<span class="cropper-line line-w" data-cropper-action="w"></span>' + '<span class="cropper-line line-s" data-cropper-action="s"></span>' + '<span class="cropper-point point-e" data-cropper-action="e"></span>' + '<span class="cropper-point point-n" data-cropper-action="n"></span>' + '<span class="cropper-point point-w" data-cropper-action="w"></span>' + '<span class="cropper-point point-s" data-cropper-action="s"></span>' + '<span class="cropper-point point-ne" data-cropper-action="ne"></span>' + '<span class="cropper-point point-nw" data-cropper-action="nw"></span>' + '<span class="cropper-point point-sw" data-cropper-action="sw"></span>' + '<span class="cropper-point point-se" data-cropper-action="se"></span>' + '</div>' + '</div>';
 
 var cropper;
 
 function edit_image(){
   const image = document.getElementById('newPic');
   cropper = new Cropper(image, {
-    //이미지 창 크기보다 작게 축소 가능
-    viewMode: 0,
-    //cropper 바깥에서 마우스로 사진 이동
-    dragMode: 'move',
-    //cropper 비율
-    aspectRatio: 16 / 9,
+    viewMode: 0, //이미지 창 크기보다 작게 축소 가능
+    dragMode: 'move', //cropper 바깥에서 마우스로 사진 이동
+    aspectRatio: 16 / 9, //cropper 비율
     //중앙 표시, 그리드부분 밝게, 배경 체크무늬 없게
     center: false,
     highlight: false,
     background: false,
-    //초기 cropper 크기
-    autoCropArea: 1,
+    autoCropArea: 1, //초기 cropper 크기
     crop(event) {},
   });
 }
@@ -143,20 +139,18 @@ var commands = [];  //버튼 명령을 저장하는 배열
 var newCanvas = null; //자른 이미지 놓을 canvas
 var btnDisable = false; //크롭 확인 버튼 비활성화
 
-//토글버튼 이미지로 수정해야함
-function toggle_cropper(toggleBtn){
-  //toggle.classList.toggle('active');
-  if(toggleBtn.id === 'enable'){
+function toggle_cropper(){
+  if(toggleBtn.getAttribute('value') === 'enable'){
     cropper.clear();
     btnDisable = true;
-    toggleBtn.id = 'disable';
-    toggleBtn.src = "img/toggle_off.svg";
+    toggleBtn.setAttribute('value', 'disable');
+    toggleBtn.setAttribute('src', 'img/toggle_off.svg');
   }
   else{
     cropper.crop();
     btnDisable = false;
-    toggleBtn.id = 'enable';
-    toggleBtn.src = "img/toggle_on.svg";
+    toggleBtn.setAttribute('value', 'enable');
+    toggleBtn.setAttribute('src', 'img/toggle_on.svg');
   }
 }
 
@@ -170,33 +164,35 @@ function rotateRight(){
   commands.push("rotate_R");
 }
 
+function get_image_canvas(){
+  newCanvas = null;
+  newCanvas = document.createElement("canvas");
+  newCanvas.setAttribute("id", 'newCanvas');
+
+  if(is_mobile()){
+    newCanvas = cropper.getCroppedCanvas({
+      width: 297,
+      height: 359
+    });
+  }
+  else{
+    newCanvas = cropper.getCroppedCanvas({
+    width: 594,
+    height: 541
+  });
+  }
+  
+  result.appendChild(newCanvas);
+  result.style.display = "flex";
+  result.style.alignItems = "center";
+  showPic.style.display = "none";
+}
+
 function get_image(){
   if(btnDisable){return;}
   else{
-    newCanvas = null;
-    newCanvas = document.createElement("canvas");
-    newCanvas.setAttribute("id", 'newCanvas');
-
-    if(is_mobile()){
-      newCanvas = cropper.getCroppedCanvas({
-        width: 297,
-        height: 359
-      });
-    }
-    else{
-      newCanvas = cropper.getCroppedCanvas({
-      width: 594,
-      height: 541
-    });
-    }
-
-    result.appendChild(newCanvas);
-    
-    result.style.display = "flex";
-    result.style.alignItems = "center";
+    get_image_canvas();
     toggle.style.display = "none";
-    showPic.style.display = "none";
-
     btnDisable = true;
     commands.push("done");
   }
@@ -249,7 +245,6 @@ function cancelUpload(){
   //업로드 박스 다시 보이도록
   upPicBox.style.display = "block";
   picture.style.marginBottom = "30px";
-
 }
 
 function upload_image(){
